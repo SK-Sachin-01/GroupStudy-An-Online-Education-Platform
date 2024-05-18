e6fd1193fd2f9613490d89fddcfaa2ba79260522
Sachin <thesachinkumar81@gmail.com>
2024-05-19 01:30:51 +0530

    RatingAndReview.js -> models updated

--- Backend/models/RatingAndReview.js
+++ Backend/models/RatingAndReview.js
@@ -14,7 +14,13 @@ const ratingAndReviewsSchema = new mongoose.Schema(
         review:{
             type:String,
             required:true,
-        }
+        },
+        course: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: "Course",
+            index: true,
+        },
     }
 );
 
